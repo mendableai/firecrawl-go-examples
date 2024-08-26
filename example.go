4a9e3ae1cfafd3a4194bbe188c169333812bbf83
rafaelsideguide <150964962+rafaelsideguide@users.noreply.github.com>
2024-08-26 16:57:35 -0300

    fix: crawl params, commented llm for now

--- example.go
+++ example.go
@@ -25,9 +25,7 @@ func main() {
 	// Crawl a website
 	idempotencyKey := uuid.New().String() // optional idempotency key
 	crawlParams := map[string]any{
-		"crawlerOptions": map[string]any{
-			"excludes": []string{"blog/*"},
-		},
+		"excludePaths": []string{"blog/*"},
 	}
 	crawlResult, err := app.CrawlURL("mendable.ai", crawlParams, true, 2, idempotencyKey)
 	if err != nil {
@@ -40,48 +38,58 @@ func main() {
 	fmt.Println(string(jsonCrawlResult))
 
 	// LLM Extraction using JSON schema
-	jsonSchema := map[string]any{
-		"type": "object",
-		"properties": map[string]any{
-			"top": map[string]any{
-				"type": "array",
-				"items": map[string]any{
-					"type": "object",
-					"properties": map[string]any{
-						"title":       map[string]string{"type": "string"},
-						"points":      map[string]string{"type": "number"},
-						"by":          map[string]string{"type": "string"},
-						"commentsURL": map[string]string{"type": "string"},
-					},
-					"required": []string{"title", "points", "by", "commentsURL"},
-				},
-				"minItems":    5,
-				"maxItems":    5,
-				"description": "Top 5 stories on Hacker News",
-			},
-		},
-		"required": []string{"top"},
-	}
+	// jsonSchema := map[string]any{
+	// 	"type": "object",
+	// 	"properties": map[string]any{
+	// 		"top": map[string]any{
+	// 			"type": "array",
+	// 			"items": map[string]any{
+	// 				"type": "object",
+	// 				"properties": map[string]any{
+	// 					"title":       map[string]string{"type": "string"},
+	// 					"points":      map[string]string{"type": "number"},
+	// 					"by":          map[string]string{"type": "string"},
+	// 					"commentsURL": map[string]string{"type": "string"},
+	// 				},
+	// 				"required": []string{"title", "points", "by", "commentsURL"},
+	// 			},
+	// 			"minItems":    5,
+	// 			"maxItems":    5,
+	// 			"description": "Top 5 stories on Hacker News",
+	// 		},
+	// 	},
+	// 	"required": []string{"top"},
+	// }
 
-	llmExtractionParams := map[string]any{
-		"extractorOptions": firecrawl.ExtractorOptions{
-			ExtractionSchema: jsonSchema,
-			Mode:             "llm-extraction",
-		},
-		"pageOptions": map[string]any{
-			"onlyMainContent": true,
-		},
-	}
+	// llmExtractionParams := map[string]any{
+	// 	"extractorOptions": firecrawl.ExtractorOptions{
+	// 		ExtractionSchema: jsonSchema,
+	// 		Mode:             "llm-extraction",
+	// 	},
+	// 	"pageOptions": map[string]any{
+	// 		"onlyMainContent": true,
+	// 	},
+	// }
 
-	llmExtractionResult, err := app.ScrapeURL("https://news.ycombinator.com", llmExtractionParams)
-	if err != nil {
-		log.Fatalf("Failed to perform LLM extraction: %v", err)
-	}
+	// llmExtractionResult, err := app.ScrapeURL("https://news.ycombinator.com", llmExtractionParams)
+	// if err != nil {
+	// 	log.Fatalf("Failed to perform LLM extraction: %v", err)
+	// }
 
 	// Pretty print the LLM extraction result
-	jsonResult, err := json.MarshalIndent(llmExtractionResult.LLMExtraction, "", "  ")
+	// jsonResult, err := json.MarshalIndent(llmExtractionResult.LLMExtraction, "", "  ")
+	// if err != nil {
+	// 	log.Fatalf("Failed to marshal LLM extraction result: %v", err)
+	// }
+	// fmt.Println(string(jsonResult))
+
+	mapResult, err := app.MapURL("https://roastmywebsite.ai", nil)
 	if err != nil {
-		log.Fatalf("Failed to marshal LLM extraction result: %v", err)
+		log.Fatalf("Failed to map URL: %v", err)
 	}
-	fmt.Println(string(jsonResult))
+	jsonMapResult, err := json.MarshalIndent(mapResult, "", "  ")
+	if err != nil {
+		log.Fatalf("Failed to marshal map result: %v", err)
+	}
+	fmt.Println(string(jsonMapResult))
 }
